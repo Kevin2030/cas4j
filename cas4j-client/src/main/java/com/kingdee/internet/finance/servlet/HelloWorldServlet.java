abc0a14ea17651fa166cdfe8ea8597e43ea989a7
Kevin <532388066@qq.com>
2015-09-22 16:41:43 +0800

    cas4j

---
+++ cas4j-client/src/main/java/com/kingdee/internet/finance/servlet/HelloWorldServlet.java
@@ -0,0 +1,31 @@
+package com.kingdee.internet.finance.servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jasig.cas.client.authentication.AttributePrincipal;
+
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String message = "<h1>Hello World!!!</h1>";
+		Writer writer = resp.getWriter();
+		AttributePrincipal principal = (AttributePrincipal) req.getUserPrincipal();
+		Map<String, Object> attributes = principal.getAttributes();
+		Object userID = attributes.get("userID");
+		Object userName = attributes.get("userName");
+		message += "<h2>userID: " + userID + "</h2>";
+		message += "<h2>userName: " + userName + "</h2>";
+		writer.write(message);
+	}
+
+}
