d7dcb5603401bf53b58aac19866331575f8bf6be
Kevin <532388066@qq.com>
2015-09-23 15:46:36 +0800

    memecached ticket

--- cas4j-server/src/main/webapp/js/cas.js
+++ cas4j-server/src/main/webapp/js/cas.js
@@ -17,10 +17,14 @@
  * under the License.
  */
 
-var scripts = [ "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
+/*var scripts = [ "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
     "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js",
-    "https://rawgithub.com/cowboy/javascript-debug/master/ba-debug.min.js"];
+    "https://rawgithub.com/cowboy/javascript-debug/master/ba-debug.min.js"];*/
+var scripts = [ "../js/jquery.min.js",
+    "../js/jquery-ui.min.js",
+    "../js/jquery.cookie.min.js",
+    "../js/ba-debug.min.js"];
 
 head.ready(document, function() {
     head.load(scripts, resourceLoadedSuccessfully);
